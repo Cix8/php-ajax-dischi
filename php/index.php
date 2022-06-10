8b111bbf6ebdf3467bea671190ed8a8a8cfe0ff7
Cix8 <francesco.natal_fn@libero.it>
2022-06-10 15:31:53 +0200

    milestone1 -> add cards in main

--- php/index.php
+++ php/index.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 include_once __DIR__ . "/../database.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,17 +15,40 @@ include_once __DIR__ . "/../database.php";
 
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
+
     <header>
         <div class="logo-container">
-        <i class="fab fa-spotify"></i>
+            <i class="fab fa-spotify"></i>
         </div>
     </header>
 
     <main>
-        
+        <div class="container">
+            <ul>
+                <?php foreach ($database as $card) { ?>
+                    <li>
+                        <div class="inner-container">
+                            <div class="poster">
+                                <img src="<?php echo $card["poster"] ?>" alt="<?php echo $card["title"] ?>">
+                            </div>
+                            <div class="title">
+                                <h4><?php echo strtoupper($card["title"]) ?></h4>
+                            </div>
+                            <div class="author">
+                                <span><?php echo $card["author"] ?></span>
+                            </div>
+                            <div class="year">
+                                <small><?php echo $card["year"] ?></small>
+                            </div>
+                        </div>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
     </main>
 
 </body>
+
 </html>
